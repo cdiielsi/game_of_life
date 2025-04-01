19cb0e27027e6e5579b937dac31cc9dbe5af0131
Camila Di Ielsi <camila.diielsi@lambdaclass.com>
2025-04-01 18:01:30 -0300

    feature: game can be paused

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,23 @@ const HEIGHT: usize = 5;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let speed = 0.3;
+    let mut last_update = get_time();
     let mut gol = GameOfLife::new(WIDTH, HEIGHT);
+    let mut game_of_life_go = true;
     loop {
         clear_background(WHITE);
 
         gol.draw_gol_board();
-        gol.transition();
+
+        if is_key_pressed(KeyCode::Space) {
+                game_of_life_go = !game_of_life_go;
+        } 
+
+        if game_of_life_go && get_time() - last_update > speed {
+            last_update = get_time();
+            gol.transition();
+        }
 
         next_frame().await
     }
